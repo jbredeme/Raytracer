bfef4019ee05609cc44e6873f17582bc3534fbb7
jbredeme <jpb64@nau.edu>
2016-11-06 12:28:17 -0700

    Recommit, no changes implemented

--- raycaster/raycaster.h
+++ raycaster/raycaster.h
@@ -1,7 +1,7 @@
 /**
  * Author: Jarid Bredemeier
  * Email: jpb64@nau.edu
- * Date: Thursday, October 6, 2016
+ * Date: Tuesday, November 1, 2016
  * File: raycaster.h
  * Copyright © 2016 All rights reserved 
  */
